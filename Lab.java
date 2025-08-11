1d0ab7de2860a1222f8d29868da4aa7f1d0a324a
xxKrishna2609xx <krishna26sep@gmail.com>
2025-08-11 22:50:45 +0530

    Java

---
+++ Lab.java
@@ -0,0 +1,27 @@
+// public class Lab {
+//     public static void main(String[] args) { // It is a method to execute the program
+//         int a = 5; // storing a number
+//         int b = 10; // storing another number
+//         int sum = a + b; // calculating the sum of a and b
+//         System.out.println("The sum of " + a + " and " + b + " is: " + sum); // printing the result
+//     }
+// }
+
+
+
+// public class Lab {
+//     public static void main(String[] args) { // It is a method to execute the program
+//         int age = 24; // storing a number
+//         String Name = "Krishna"; // storing My Name 
+//         System.out.println(age); // Printing My Age in new line
+//         System.out.println(Name); // Printing My Name in new line
+//     }
+// }
+
+
+public class Lab{
+    public static void main(String[] args) {
+        int a = 5 ;
+        
+    }
+}
---
+++ datatype.java
@@ -0,0 +1,28 @@
+// public class datatype {
+//     public static void main(String[] args) {
+//         System.out.println("int = 4 bytes");
+//         System.out.println("Long = 8 bytes");
+//         System.out.println("Float = 4 bytes");
+//         System.out.println("Double = 8 bytes");
+//         System.out.println("Boolean = 1 Bit");
+//         System.out.println("Char = 2 bytes");
+//     }
+// }
+
+
+// import java.util.*;
+// public class datatype {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         System.out.println(a);
+//     }
+// }
+
+
+
+public class datatype {
+    public static void main(String[] args) {
+        System.out.println(Byte.MIN_VALUE + "TO" + Byte.MAX_VALUE);
+    }
+}
