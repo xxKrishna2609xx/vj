0b8981e758eefa31dbccaf6a492df2c8dab8ea59
xxKrishna2609xx <krishna26sep@gmail.com>
2025-08-19 15:23:15 +0530

    container config done

--- ifelse.java
+++ ifelse.java
@@ -1,11 +1,32 @@
-public class ifelse {
+// public class ifelse {
+//     public static void main(String[] args) {
+//                 int age = 18;
+//             if (age==18){
+//                 System.out.println("vote");
+//             }
+//             else{
+//                 System.out.println("No");
+//             }
+//     }
+// }
+
+
+public class ifelse{
     public static void main(String[] args) {
-                int age = 18;
-            if (age==18){
-                System.out.println("vote");
-            }
-            else{
-                System.out.println("No");
-            }
+        int day = 12;
+        switch (day){
+            case 1: System.out.println("Monday");   break;
+            case 2: System.out.println("Tuesday");  break;
+            case 3: System.out.println("Wednesday");break;
+            case 4: System.out.println("Thursday"); break;
+            default: System.out.println("None");    break;
+        }
     }
 }
+
+
+// public class ifelse{
+//     public static void main(String[] args) {
+//         int a = 
+//     }
+// }
