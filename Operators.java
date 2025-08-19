b337a6049da7b3bf93cde3df84530f0f48071f07
xxKrishna2609xx <krishna26sep@gmail.com>
2025-08-19 14:38:57 +0530

    container config done

---
+++ Operators.java
@@ -0,0 +1,32 @@
+// public class Operators {
+//     public static void main(String[] args) {
+//         int a=8 , b=7;
+//         System.out.println(a==b);
+//         System.out.println(++a);
+//         System.out.println(a++);
+//         System.out.println(a);
+//     }
+// }
+
+
+// public class Operators {
+//     public static void main(String[] args) {
+//         int a = 5;
+//         int b = 2;
+//         System.out.println(a >> b); // Right shift
+//     }
+// }
+
+
+// public class Operators {
+//     public static void main(String[] args) {
+//         int age = 18;
+//             if (age==18){
+//                 System.out.println("vote");
+//             }
+//             else{
+//                 System.out.println("No");
+//             }
+//     }
+// }
+
---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String[] args) {
+                int age = 18;
+            if (age==18){
+                System.out.println("vote");
+            }
+            else{
+                System.out.println("No");
+            }
+    }
+}
