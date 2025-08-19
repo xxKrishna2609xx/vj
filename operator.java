24ed64025bd00ea1d42265a28cceb925116f16cc
xxKrishna2609xx <krishna26sep@gmail.com>
2025-08-19 11:54:39 +0530

    container config done

---
+++ operator.java
@@ -0,0 +1,16 @@
+// public class operator {
+//     public static void main(String[] args) {
+//         int a=8 , b=7;
+//         System.out.println(a==b);
+//         System.out.println(++a);
+//         System.out.println(a++);
+//         System.out.println(a);
+//     }
+// }
+
+
+public class operator {
+    public static void main(String[] args) {
+        
+    }
+}
