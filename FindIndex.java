b60df8bf994b75b2eda3f3d1720677b7f73f5fb2
Krishna <krishna26sep@gmail.com>
2025-10-16 09:07:15 +0530

    container config done

---
+++ FindIndex.java
@@ -0,0 +1,61 @@
+// import java.util.*;
+
+// public class PascalTriangle {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 0; i < n; i++) {
+//             int val = 1;
+//             for (int j = 0; j <= i; j++) {
+//                 System.out.print(val + " ");
+//                 val = val * (i - j) / (j + 1);
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+
+
+// import java.util.Scanner;
+// public class Patterns {
+
+// 	public static void main(String[] args) {
+// 		Scanner sc = new Scanner(System.in);
+// 		int n=sc.nextInt();
+// 		for(int row=0;row<n;row++) {
+// 			for(int col=0;col<=row;col++) {
+// 				System.out.print("* ");
+// 			}
+// 			System.out.println();
+// 		}
+// 		sc.close();
+// 	}
+// }
+
+
+
+// import java.util.*;
+//  public class  sumofarray{
+// 	public static void main(String[] args){
+// 		Scanner sc = new Scanner (System.in);
+// 		int arr[] = {12,1,34,3,6,9};
+
+// 		int sum = 0;
+// 		for(int i=0 ; i<arr.length ; i++){
+// 			sum += arr[i];
+// 		}
+// 		System.out.println(sum);
+// 		sc.close();
+// 	}
+// }
+
+
+
+
+import java.util.*;
+public class FindIndex{
+    
+}
