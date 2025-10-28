6100ef6a3713ebf43916c25014077a28ea2e7f38
Krishna <krishna26sep@gmail.com>
2025-10-28 11:29:09 +0530

    java 28Oct

--- main.java
+++ main.java
@@ -1,19 +1,21 @@
-class Animal{
-    void eat(){
-        System.out.println("Animal eats");
-    }
-}
+// class Animal{
+//     void eat(){
+//         System.out.println("Animal eats");
+//     }
+// }
 
-class Dog extends Animal{
-    void bark(){
-        System.out.println("Dog is Barking");
-    }
-}
+// class Dog extends Animal{
+//     void bark(){
+//         System.out.println("Dog is Barking");
+//     }
+// }
 
-public class main {
-    public static void main(String[]args){
-        Dog d = new Dog();
-        d.eat();
-        d.bark();
-    }
-}
+// public class main {
+//     public static void main(String[]args){
+//         Dog d = new Dog();
+//         d.eat();
+//         d.bark();
+//     }
+// }
+
+
